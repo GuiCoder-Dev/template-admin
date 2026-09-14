a5da1e08cf33e80f121ee35b2e25e0905b61e5f4
GuiCoder-Dev <guimalagutti07@gmail.com>
2026-09-14 14:46:54 -0300

    criação da tela de login/cadastro

--- app/notificacoes/page.tsx
+++ app/notificacoes/page.tsx
@@ -6,7 +6,7 @@ export default function Notificacoes() {
 
   return (
     <Layout titulo="Notificações" subtitulo="Aqui você irá gerenciar as suas notificações">
-     
+     <h1>Notificações</h1>
     </Layout>
   )
 
---
+++ components/auth/AuthInput.tsx
@@ -0,0 +1,32 @@
+interface AuthInputProps{
+    label: string
+    valor: any
+    tipo?: "text" | "email" | "password"
+    obrigatorio?: boolean
+    valorMudou: (novoValor: any) => void
+}
+
+export default function AuthInput(props: AuthInputProps){
+
+    return (
+        <div className="flex flex-col mt-4">
+            <label>{props.label}</label>
+            <input 
+                type={props.tipo ?? "text"}
+                required={props.obrigatorio}
+                value={props.valor}
+                onChange={e => props.valorMudou?.(e.target.value)}
+
+                className={`
+                px-4 py-3
+                rounded-lg    
+                bg-gray-200
+                mt-2
+                border focus:border-blue-500 focus:bg-white
+                focus:outline-none
+                `}
+            />
+        </div>
+    )
+
+}
